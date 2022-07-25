9149e55c8783eded4193146f57739b0bd371885e
almost <3370084577@qq.com>
2022-07-25 16:24:59 +0800

    Initial commit

--- reflect/src/com/lyg/d1_reflect/Student.java
+++ reflect/src/com/lyg/d1_reflect/Student.java
@@ -8,4 +8,18 @@ public class Student {
     private String name;
     private Integer age;
     private String Number;
+    private String sex;
+    private String hobby;
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", Number='" + Number + '\'' +
+                ", sex='" + sex + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
 }
