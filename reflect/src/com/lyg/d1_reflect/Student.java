75a640a47a88466e15689922c37eda550acd76eb
almost <3370084577@qq.com>
2022-07-25 16:20:23 +0800

    Initial commit

---
+++ reflect/src/com/lyg/d1_reflect/Student.java
@@ -0,0 +1,11 @@
+package com.lyg.d1_reflect;
+
+/**
+ * @author almost
+ * @date 2022/7/25 16:16
+ */
+public class Student {
+    private String name;
+    private Integer age;
+    private String Number;
+}
